c64af2af11b034db3ec416006ad1aea75f13fee3
Rob Romito <rob@robertromito.com>
2021-12-02 08:58:37 -0500

    2021 day 2

---
+++ 2021/day2/part1.py
@@ -0,0 +1,15 @@
+with open("input.txt") as input:
+    movements = input.readlines()
+
+location = [0, 0]
+
+for m in movements:
+    match [x := m.split(), x[0], int(x[1])]:
+        case [_, "forward", i]:
+            location[0] += i
+        case [_, "down", i]:
+            location[1] += i
+        case [_, "up", i]:
+            location[1] -= i
+
+print(f"location is {location}. product is {location[0] * location[1]}")
