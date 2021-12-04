7834b447c8621d32652e466de2da4fd18ea6ae6c
Rob Romito <rob@robertromito.com>
2021-12-04 08:31:57 -0500

    2021 day 3 final

--- 2021/day3/part1.py
+++ 2021/day3/part1.py
@@ -1,22 +1,35 @@
-gamma_rate = 0  # most common bits in column
-epsilon_rate = 0  # opposite of gamma_rate
-
 with open("input.txt") as input:
-    measurements = input.read().splitlines()  # drops \n from each line
+    MEASUREMENTS = input.read().splitlines()  # drops \n from each line
+
+INPUT_MEASUREMENT_COUNT = len(MEASUREMENTS)
+DIGITS_PER_MEASUREMENT = len(MEASUREMENTS[0])
+
+
+def get_final_rate(digits):
+    return int(bin(int("".join(digits), 2)), 2)
+
+
+def select_digits(digits, selector):
+    # int(truthy value) returns 1 if truthy, 0 if falsy
+    return [str(int(selector(d))) for d in digits]
+
 
 # Since we're dealing with 0 and 1, we might be able to use addition to get the counts
 # If there are more 1's than 0's, the sum of the column will be greater than the number
 # of measurements / 2
 
-number_of_measurements = len(measurements)
-number_of_digits = len(measurements[0])
-sums = [0] * number_of_digits
-for m in measurements:
-    sums = list(map(sum, zip(sums, [int(d) for d in m])))
-g = [str(int(d >= (number_of_measurements/2))) for d in sums]
-e = [str(int(d < (number_of_measurements/2))) for d in sums]
-gamma_rate = int(bin(int("".join(g), 2)), 2)
-epsilon_rate = int(bin(int("".join(e), 2)), 2)
+digit_sums = [0] * DIGITS_PER_MEASUREMENT
+for m in MEASUREMENTS:
+    digit_sums = list(map(sum, zip(digit_sums, [int(d) for d in m])))
 
+g = select_digits(digit_sums, lambda d: d >= (INPUT_MEASUREMENT_COUNT / 2))
+e = select_digits(digit_sums, lambda d: d < (INPUT_MEASUREMENT_COUNT / 2))
 
-print(f"Power consumption is {gamma_rate * epsilon_rate} [{gamma_rate}g * {epsilon_rate}e]")
+gamma_rate = get_final_rate(g)
+epsilon_rate = get_final_rate(e)
+
+print(
+    f"Power consumption is "
+    f"{gamma_rate * epsilon_rate} "
+    f"[{gamma_rate}g * {epsilon_rate}e]"
+)
--- 2021/day3/part2.py
+++ 2021/day3/part2.py
@@ -1,22 +1,46 @@
-gamma_rate = 0  # most common bits in column
-epsilon_rate = 0  # opposite of gamma_rate
+import math
+import operator
+
+o2_check = operator.ge
+co2_check = operator.lt
 
 with open("input.txt") as input:
-    measurements = input.read().splitlines()  # drops \n from each line
-
-# Since we're dealing with 0 and 1, we might be able to use addition to get the counts
-# If there are more 1's than 0's, the sum of the column will be greater than the number
-# of measurements / 2
-
-number_of_measurements = len(measurements)
-number_of_digits = len(measurements[0])
-sums = [0] * number_of_digits
-for m in measurements:
-    sums = list(map(sum, zip(sums, [int(d) for d in m])))
-g = [str(int(d >= (number_of_measurements/2))) for d in sums]
-e = [str(int(d < (number_of_measurements/2))) for d in sums]
-gamma_rate = int(bin(int("".join(g), 2)), 2)
-epsilon_rate = int(bin(int("".join(e), 2)), 2)
+    MEASUREMENTS = input.read().splitlines()  # drops \n from each line
 
 
-print(f"Power consumption is {gamma_rate * epsilon_rate} [{gamma_rate}g * {epsilon_rate}e]")
+INPUT_MEASUREMENT_COUNT = len(MEASUREMENTS)
+DIGITS_PER_MEASUREMENT = len(MEASUREMENTS[0])
+
+
+def get_final_rate(digits):
+    return int(bin(int("".join(digits), 2)), 2)
+
+
+def scan_measurements(measurements, comparison_func):
+    measurement_count = len(measurements)
+    compare_to = math.ceil(measurement_count / 2)
+    sums = [0] * DIGITS_PER_MEASUREMENT
+    for m in measurements:
+        sums = list(map(sum, zip(sums, [int(d) for d in m])))
+    final = [str(int(comparison_func(d, compare_to))) for d in sums]
+    return final
+
+
+def get_life_support_measurement(comparison_func):
+    remaining_measurements = MEASUREMENTS
+    for i in range(DIGITS_PER_MEASUREMENT):
+        val = scan_measurements(remaining_measurements, comparison_func)
+        remaining_measurements = [m for m in remaining_measurements if m[i] == val[i]]
+        if len(remaining_measurements) == 1:
+            break
+    return remaining_measurements[0]
+
+
+o2_generator_rate = get_final_rate(get_life_support_measurement(o2_check))
+co2_scrubber_rate = get_final_rate(get_life_support_measurement(co2_check))
+
+print(
+    f"Life support rating is "
+    f"{o2_generator_rate * co2_scrubber_rate} "
+    f"[{o2_generator_rate}o2 * {co2_scrubber_rate}co2]"
+)
